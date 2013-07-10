ba2c43a95acf1096e0790e0d368e5cf218bcc525
Sweet Song <shihui.sweet.song@gmail.com>
2013-07-10 14:14:11 -0400

    modified the run_emr_job.sh to run the PreFormat for all memes

--- src/twitter-memes/PreFormat/run_emr_job.sh
+++ src/twitter-memes/PreFormat/run_emr_job.sh
@@ -1,20 +1,19 @@
+S3_BUCKET=memes-bson
 
-elastic-mapreduce --create --jobflow PreFormat5 \
-    --instance-type m1.small \
-    --num-instances 1 \
+elastic-mapreduce --create --jobflow PreFormat8 \
+    --name "All Memes = Preformat 8" \
+    --instance-type m1.xlarge \
+    --num-instances 8 \
     --bootstrap-action s3://$S3_BUCKET/emr-bootstrap.sh \
     --log-uri s3://$S3_BUCKET/logs \
-    --jar s3://$S3_BUCKET/PreFormat.jar \
-    #--arg -D --arg mongo.job.input.format=com.mongodb.hadoop.BSONFileInputFormat \
-    --arg -D --arg mapred.input.dir=s3n://$S3_BUCKET/input/5.bson \
-    #--arg -D --arg mongo.job.mapper=com.mongodb.hadoop.examples.enron.EnronMailMapper \
-    #--arg -D --arg mongo.job.output.key=com.mongodb.hadoop.examples.enron.MailPair \
-    #--arg -D --arg mongo.job.output.value=org.apache.hadoop.io.IntWritable \
-    --arg mongo.job.partitioner= \
-    #--arg -D --arg mongo.job.reducer=com.mongodb.hadoop.examples.enron.EnronMailReducer \
-    --arg mongo.job.sort_comparator= \
-    --arg mongo.job.background= \
-    --arg -D --arg mapred.output.file=s3n://$S3_BUCKET/output/12.bson 
-    #--arg -D --arg mongo.job.output.format=com.mongodb.hadoop.BSONFileOutputFormat \
-    #--arg -D --arg mapred.child.java.opts=-Xmx2048m
-    #--arg -D --arg mapred.task.profile=true \
+    --jar s3://$S3_BUCKET/PreFormatDir.jar \
+    --arg -D --arg mongo.job.input.format=com.mongodb.hadoop.BSONFileInputFormat \
+    --arg -D --arg mapred.input.dir=s3n://$S3_BUCKET/dump/twitter/original \
+    --arg -D --arg mongo.job.mapper=pre.PreFormatMapper \
+    --arg -D --arg mongo.job.reducer=pre.PreFormatReducer \
+    --arg -D --arg mongo.job.output.key=org.apache.hadoop.io.Text \
+    --arg -D --arg mongo.job.output.value=com.mongodb.hadoop.io.BSONWritable \
+    --arg -D --arg bson.pathfilter.class=com.mongodb.hadoop.BSONPathFilter \
+    --arg -D --arg mapred.output.dir=s3n://$S3_BUCKET/output/originalMemes \
+    --arg -D --arg mongo.job.output.format=com.mongodb.hadoop.BSONFileOutputFormat \
+    --arg -D --arg bson.output.build_splits=true
