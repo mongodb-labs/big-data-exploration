a1a775931ba80346da1150b84ee272c28b6c3723
Sweet Song <shihui.sweet.song@gmail.com>
2013-07-29 01:58:51 -0400

    Finished revamping the PageRan-Flights directory

--- PageRank-Flights/src/airportsInfo.js
+++ PageRank-Flights/src/airportsInfo.js
@@ -6,21 +6,21 @@
 //              , airportCity: "New York, NY"}
 var getAirportsInfo = function() {
     var airportInfo = {}
-    , n = 40; // last iteration of PageRank
+    , n = 20; // last iteration of PageRank
     
     var airportNodes = db["fpg_"+n].find()
     , flightInfo, id, airportNode;
 
     while (airportNodes.hasNext()) {
-	airportNode = airportNodes.next();
-	id = airportNode["_id"];
-	airportInfo[id] = {"pg" : airportNode.value.pg};
-	flightInfo = db.flights.findOne({"origAirportId" : parseInt(id)});
+        airportNode = airportNodes.next();
+        id = airportNode["_id"];
+        airportInfo[id] = {"pg" : airportNode.value.pg};
+        flightInfo = db.flights.findOne({"origAirportId" : parseInt(id)});
 
-	airportInfo[id]["airportCode"] = flightInfo["origAirport"];
-	airportInfo[id]["airportState"] = flightInfo["origState"];
-	airportInfo[id]["airportStateId"] = flightInfo["origStateId"];
-	airportInfo[id]["airportCity"] = flightInfo["origCity"];
+        airportInfo[id]["airportCode"] = flightInfo["origAirport"];
+        airportInfo[id]["airportState"] = flightInfo["origState"];
+        airportInfo[id]["airportStateId"] = flightInfo["origStateId"];
+        airportInfo[id]["airportCity"] = flightInfo["origCity"];
     }
 
     return airportInfo;
